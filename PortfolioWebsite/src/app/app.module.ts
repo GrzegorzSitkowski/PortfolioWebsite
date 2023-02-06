cf7c428e47b75783e7877d9716e5bd1ec5d1d100
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-06 18:20:47 +0100

    Created component Contact

--- PortfolioWebsite/src/app/app.module.ts
+++ PortfolioWebsite/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalInformationComponent } from './Components/personal-information/personal-information.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalInformationComponent
+    PersonalInformationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

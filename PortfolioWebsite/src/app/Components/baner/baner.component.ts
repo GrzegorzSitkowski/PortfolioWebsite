30c04fb905d1e8bbe3720d44193f2e0e8e1db584
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-06 19:52:47 +0100

    Installed angular cdk

--- PortfolioWebsite/src/app/Components/baner/baner.component.ts
+++ PortfolioWebsite/src/app/Components/baner/baner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-baner',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BanerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breakpointObserve: BreakpointObserver) { }
 
   ngOnInit(): void {
   }

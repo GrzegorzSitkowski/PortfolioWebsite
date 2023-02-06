3d6e47907cd576fcf6563e20a39e831d87797db7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-06 18:19:30 +0100

    Created component PersonalInformation

--- PortfolioWebsite/src/app/app.module.ts
+++ PortfolioWebsite/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalInformationComponent } from './Components/personal-information/personal-information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInformationComponent
   ],
   imports: [
     BrowserModule,

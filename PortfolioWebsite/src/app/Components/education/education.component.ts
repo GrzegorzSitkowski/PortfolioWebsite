ba42cc620f5e8438c90aed6608cb993cc89665b8
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-08 17:54:51 +0100

    Implemented Education

--- PortfolioWebsite/src/app/Components/education/education.component.ts
+++ PortfolioWebsite/src/app/Components/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Education } from 'src/app/models/models';
 
 @Component({
   selector: 'app-education',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+
+  educationList: Education[] = [
+    {
+      school: 'Vana Vani School',
+      course: 'IT',
+      duration: '2007 - 2011',
+    }
+  ];
 
   constructor() { }
 

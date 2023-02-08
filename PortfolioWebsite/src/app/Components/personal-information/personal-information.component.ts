b145cd050347e5b100f4889373ced40eac9e905b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-08 17:10:41 +0100

    Added personal data

--- PortfolioWebsite/src/app/Components/personal-information/personal-information.component.ts
+++ PortfolioWebsite/src/app/Components/personal-information/personal-information.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalInformationComponent implements OnInit {
 
+  myData: string[][] = [
+    ['Name', 'Grzegorz Sitkowski'],
+    ['DOB', '26/04/1995']
+  ];
+
+  aboutMe: string[] = [
+    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills,',
+    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills,',
+    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills.'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {

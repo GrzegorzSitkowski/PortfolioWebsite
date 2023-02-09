e0e708991c17b47485ee80e3564c0a55137de5c0
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-09 15:17:18 +0100

    Implemented section of projects

--- PortfolioWebsite/src/app/Components/projects/projects.component.ts
+++ PortfolioWebsite/src/app/Components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/models';
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects: Project[] = [
+
+    {
+      title: 'SporteNotepadMVC',
+      technologies: 'C#, MVC, Microsoft SQL Server, Entity Framework, Unit Tests',
+      description: ['Application to save your own records of running.']
+    }
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {

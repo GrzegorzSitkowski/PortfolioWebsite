fff4d1a4d0cf4b43389751cde9f1b67512e3b7a6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-28 22:11:51 +0100

    Updated projects

--- PortfolioWebsite/src/app/Components/projects/projects.component.ts
+++ PortfolioWebsite/src/app/Components/projects/projects.component.ts
@@ -9,11 +9,17 @@ import { Project } from 'src/app/models/models';
 export class ProjectsComponent implements OnInit {
 
   projects: Project[] = [
-
     {
-      title: 'SporteNotepadMVC',
-      technologies: 'C#, MVC, Microsoft SQL Server, Entity Framework, Unit Tests',
-      description: ['Application to save your own records of running.']
+      title: 'Hotel System',
+      technologies: 'ASP.NET, REST API, .NET Core 5.0, EF Core, MediatR, CQRS, XUnit tests, MSSQL, HTML5, CSS3, Bootstrap, ,TypeScript, Angular',
+      description: ['Web application to manage reservations of users and rooms.',
+      'More: https://github.com/GrzegorzSitkowski/HotelSystem']
+    },
+    {
+      title: 'Running and workouts management web application',
+      technologies: 'ASP.NET MVC, .NET Core 5.0, EF Core, XUnit tests, MSSQL, HTML5, CSS3, Bootstrap',
+      description: ['Web application to save your run competitions, workouts, photos and goals. You can management your running and track your progress.',
+      'More: https://github.com/GrzegorzSitkowski/SportNotepadMVC']
     }
   ]
   

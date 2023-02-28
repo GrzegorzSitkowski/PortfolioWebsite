ce7e5cd35547bd603729e137348be425e902aa8f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2023-02-28 22:30:02 +0100

    Updated personal information

--- PortfolioWebsite/src/app/Components/personal-information/personal-information.component.ts
+++ PortfolioWebsite/src/app/Components/personal-information/personal-information.component.ts
@@ -13,9 +13,9 @@ export class PersonalInformationComponent implements OnInit {
   ];
 
   aboutMe: string[] = [
-    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills,',
-    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills,',
-    'I am search job as Junior .Net Developer. I like learn new things and to develop my skills.'
+    'I am search job as Junior .Net Developer. I live in Łódź (current), I am open to relocate.',
+    'In my opinion developing your skills is dangerous. I not like to standing in one place. So I try to develop my skills and learn new things.',
+    'I like to create something out of nothing. I would like to work on projects that help for people in every part of their lives.'
   ];
 
   constructor() { }
